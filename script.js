d378feb17c68ba9e737f0dbb0c229e11e95dc34d
Elizabeth225156 <elizabeth.elden@mcvts.org>
2026-03-13 13:28:31 -0400

    Added more files

---
+++ script.js
@@ -0,0 +1,6 @@
+function clicked(){
+    let data_name = document.getElementById("name").value;
+    let data_id = document.getElementById("id").value;
+    document.getElementsByClassName("text_input")[0].value='';
+    document.getElementsByClassName("text_input")[1].value='';
+}
