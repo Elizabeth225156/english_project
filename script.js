62e8dfb98ae883e0cb7d9614206ce6003458401b
Elizabeth225156 <elizabeth.elden@mcvts.org>
2026-03-18 11:27:48 -0400

    Did stuff

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+function fetchJSONData() {
+            fetch('./data.json')
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! Status: ${response.status}`);
+                    }
+                    return response.json();  
+                })
+                .then(data => console.log(data))  
+                .catch(error => console.error('Failed to fetch data:', error)); 
+        }
+        fetchJSONData();  
+
+
 function clicked(){
     let data_name = document.getElementById("name").value;
     let data_id = document.getElementById("id").value;
     document.getElementsByClassName("text_input")[0].value='';
     document.getElementsByClassName("text_input")[1].value='';
 }
+
+let erisData = fetchJSONData();
+console.log(erisData);
